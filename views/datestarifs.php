7e84edc1f4ca7c496a7faf5c64af77cd1fe5bede
Antoine <antoinepascualmail@gmail.com>
2022-03-19 19:59:41 +0100

    Ajout des tarifs sur la page de tarifs

--- views/datestarifs.php
+++ views/datestarifs.php
@@ -15,27 +15,146 @@
 <body>
 
 <?php include 'navbar.php'; ?>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
 
 
 
 <div class="container" align="center">
 
+<h1>Dates d'ouvertures :</h1>
+<p class="lead">Ouvert tous les jours, semaine, week-end et jours fériés ( du 21 mars au 11 novembre 2022 )</p>
+
+<table class="table table-striped">
+<tr>
+	<th>Du 21 Mars au 30 Juin</th>
+	<th>14h à 18h</th>
+	
+</tr>
+<tr>
+	<th>Juillet - Août</th>
+	<th>10 à 19h</th>
+	
+</tr>
+<tr>
+	<th>Du 1er Septembre au 11 Novembre</th>
+	<th>14h à 18h  </th>
+	
+</tr>
+</table>
+
+<hr>
+<p class="lead">Ouvert tous les jours fériés</p>
+
+<table class="table table-striped">
+<tr>
+	<th>18 Avril</th>
+	<th>Ouvert 14h à 18h</th>
+	
+</tr>
+<tr>
+	<th>1er Mai</th>
+	<th>Ouvert 14h à 18h</th>
+	
+</tr>
+<tr>
+	<th>08 Mai</th>
+	<th>Ouvert de 14h à 18h</th>
+	
+</tr>
+<tr>
+	<th>26 Mai</th>
+	<th>Ouvert de 14h à 18h<</th>
+	
+</tr>
+<tr>
+	<th>06 Juin</th>
+	<th>Ouvert de 14h à 18h<</th>
+	
+</tr>
+<tr>
+	<th>14 Juillet</th>
+	<th>Ouvert de 10h à 19h</th>
+	
+</tr>
+<tr>
+	<th>15 Août</th>
+	<th>Ouvert de 10h à 19h</th>
+	
+</tr>
+<tr>
+	<th>1er Novembre</th>
+	<th>Ouvert de 10h à 19h</th>
+	
+</tr>
+<tr>
+	<th>11 Novembre</th>
+	<th>Ouvert de 10h à 19h</th>
+	
+</tr>
+</table>
+<p class="lead">Visite, Présentation et Animation pendant une à deux heures 
+avec un Guide-Animateur en Costume</p>
+
+<hr>
+
+<h1>Tarifs :</h1>
+
+
+<section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+      <!-- Free Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h6 class="card-title text-muted text-uppercase text-center">Enfant de 3 à 12 ans</h6>
+            <hr>
+            <h6 class="card-price text-center">6€</h6>
+            <hr>
+            <ul class="fa-ul">
+              
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+      <!-- Plus Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h6 class="card-title text-muted text-uppercase text-center">Enfant de 12 à 17 ans</h6>
+            <hr>
+            <h6 class="card-price text-center">9€</h6>
+            <hr>
+            <ul class="fa-ul">
+              
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+      <!-- Pro Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h6 class="card-title text-muted text-uppercase text-center">Adulte</h6>
+            <hr>
+            <h6 class="card-price text-center">9€</h6>
+            <hr>
+            <ul class="fa-ul">
+             
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
 
 
-</div>
-
-
-
-
+</div> <!-- container -->
 </body>
 </html>
