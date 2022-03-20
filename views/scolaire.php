965819e84b5a55540e115a5abf2d0ad6167535dc
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-20 10:27:57 +0100

    Début page scolaire

---
+++ views/scolaire.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+    <link href="/medieval/views/css/vendor/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI Pro -->
+    <link href="/medieval/views/css/flat-ui-pro.css" rel="stylesheet">
+</head>
+<style>
+
+</style>
+<body>
+
+<?php include 'navbar.php'; ?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+<div class="container" align="center">
+
+
+
+
+
+
+</div>
+
+
+
+
+</body>
+</html>
