f6a526187a1923919166fe5622bac072ca7e8344
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-20 12:04:04 +0100

    Update contact.php

--- views/contact.php
+++ views/contact.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title></title>
+	<title>Contactez nous</title>
     <link href="/medieval/views/css/vendor/bootstrap.min.css" rel="stylesheet">
 
     <!-- Loading Flat UI Pro -->
@@ -21,8 +21,24 @@
 
 <div class="container" align="center">
 
-
-
+<h1>Adresse :</h1>
+<p class="lead">
+	Camp de Siège Médiéval <br>
+La Cité des Machines <br>
+32100 Larressingle
+</p>
+<hr>
+<h1>Téléphone :</h1>
+<p class="lead">
+05.62.68.33.88 
+<br>
+06.06.75.47.19
+</p>
+<hr>
+<h1>Courriel :</h1>
+<p class="lead">
+<a href="mailto:campsiege@gmail.com ">campsiege@gmail.com </a>
+</p>
 
 
 
