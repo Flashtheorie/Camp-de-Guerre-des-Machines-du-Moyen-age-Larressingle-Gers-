8222fe04c7cce0185c133371f2e0475eaf9ee096
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-20 11:26:19 +0100

    Page accès

--- index.php
+++ index.php
@@ -20,6 +20,13 @@ switch ($request) {
      case '/medieval/scolaire' :
         require __DIR__.'/views/scolaire.php';
         break;
+
+     case '/medieval/acces' :
+        require __DIR__.'/views/acces.php';
+        break;
+
+
+
     default:
         http_response_code(404);
         require 'views/404.php';
---
+++ views/acces.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Accés</title>
+    <link href="/medieval/views/css/vendor/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI Pro -->
+    <link href="/medieval/views/css/flat-ui-pro.css" rel="stylesheet">
+</head>
+<style>
+
+</style>
+<body>
+
+<?php include 'navbar.php'; ?>
+
+
+
+
+
+<div class="container" align="center">
+
+<img src="./img/acces1.png" alt="" class="img-thumbnail">
+
+<p class="lead">
+	Larressingle est l'un des plus beaux villages fortifiés de France. Situé dans 
+le nord du département du Gers, à 40 km d'Agen et environ 100 km de Toulouse, 
+dans cette Gascogne historique, riche en châteaux et belles épopées. Son accès 
+est aisé (voir carte ci-contre) et sa visite peut facilement s'insérer dans 
+une découverte plus large des richesses de cette région.
+
+Le Camp de Siège Médiéval ne se situe pas à l'intérieur des remparts de la cité.
+L'entrée du Camp se situe côté nord du parking. S'avancer jusqu'à la Bastille en bois.
+</p>
+
+<hr>
+<img src="./img/acces2.jpeg" alt="" class="img-thumbnail">
+<p class="lead">Le Camp de Siège Médiéval ne se situe pas à l'intérieur des remparts de la cité.
+L'entrée du Camp se situe côté nord du parking.
+S'avancer jusqu'à la Bastille en bois.</p>
+
+
+
+
+
+
+
+
+
+
+
+</div> <!-- container -->
+
+
+
+
+</body>
+</html>
